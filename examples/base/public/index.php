b82f8d10e78fa73bd784e50099fb06ac4ddde2d1
Walter Dal Mut <walter.dalmut@gmail.com>
2012-07-01 09:15:17 +0200

    Base example

---
+++ examples/base/public/index.php
@@ -0,0 +1,31 @@
+<?php 
+require_once realpath(__DIR__ . '/../../src/Loader.php');
+Loader::register();
+
+$app = new Application();
+
+// By default but more clear
+$app->setControllerPath(__DIR__ . '/../controllers');
+
+$app->bootstrap("view", function(){
+    $view = new View();
+    $view->setViewPath(__DIR__ . '/../views');
+    
+    return $view;
+});
+
+$app->bootstrap("layout", function(){
+    $layout = new Layout();
+    $layout->setViewPath(__DIR__ . '/../layouts');
+    
+    $layout->addHelper("title", function($part = false){
+        static $parts = array();
+        static $delimiter = ' :: ';
+    
+        return ($part === false) ? implode($delimiter, $parts) : $parts[] = $part;
+    });
+    
+    return $layout;
+});
+
+$app->run();
