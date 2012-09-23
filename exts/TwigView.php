27cc23e75bd7ff8ce89181887b03c6b2ae97c0f2
Walter Dal Mut <walter.dalmut@gmail.com>
2012-09-23 22:17:48 +0200

    Added twig view

---
+++ exts/TwigView.php
@@ -0,0 +1,19 @@
+<?php
+class TwigView extends View
+{
+    private $_twig;
+
+    public function initTwig()
+    {
+        $loader = new Twig_Loader_Filesystem($this->getViewPaths());
+        $this->_twig = new Twig_Environment($loader);
+    }
+
+    public function render($filename, $data = false)
+    {
+        $template = $this->_twig->loadTemplate($filename);
+        $data = (is_array($data)) ? array_merge($this->_getData(), $data) : $this->_getData();
+
+        return $template->render($data);
+    }
+}
